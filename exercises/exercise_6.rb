f60452df2d51ae928b418b8bd77c212005a4e3c8
Albert Ho <albertcyi.ho@gmail.com>
2022-03-07 16:43:13 -0800

    One=to-many associations - create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# One-to-many association
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 30)
+@store1.employees.create(first_name: "Ron", last_name: "Weasly", hourly_rate: 12)
+@store1.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Draco", last_name: "Malfoy", hourly_rate: 10)
+@store2.employees.create(first_name: "Severus", last_name: "Snape", hourly_rate: 20)
+@store2.employees.create(first_name: "Rubeus", last_name: "Hagrid", hourly_rate: 30)
